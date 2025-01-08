0d76cac85e751bd4d8a29191aa97f98207ad0d5c
Strxct <60505346+calvinhofman@users.noreply.github.com>
2025-01-08 15:09:11 +0100

    First init

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layout.base')
 @section('content')
 <div class="w-1/2 mx-auto mt-5">
-    <h1 class="text-center">Create Student</h1>
+    <h1 class="text-center">Voeg en nieuw kind toe</h1>
     <form action="{{ route('student.store') }}" method="POST" class="max-w-md mx-auto">
         @csrf
 
-         <!-- firstname -->
+        <!-- firstname -->
         <div class="mb-4">
-            <label for="firstname" class="block font-medium text-gray-700">First Name</label>
-            <input type="text" name="firstname" id="firstname" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
+            <label for="name" class="block font-medium text-gray-700">Naam</label>
+            <input type="text" name="name" id="name" placeholder="Naam" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
             @error('firstname')
-                <span class="text-red-500 text-sm">{{ $message }}</span>
+            <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
@@ -18,39 +18,45 @@
         <!-- lastname -->
 
         <div class="mb-4">
-            <label for="lastname" class="block font-medium text-gray-700">Last Name</label>
-            <input type="text" name="lastname" id="lastname" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
+            <label for="parentMail" class="block font-medium text-gray-700">email ouders</label>
+            <input type="text" name="parentMail" id="parentMail" placeholder="ouderemail@gmail.com" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
             @error('lastname')
-                <span class="text-red-500 text-sm">{{ $message }}</span>
+            <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
 
-         <!-- email -->
+        <!-- email -->
         <div class="mb-4">
-            <label for="email" class="block font-medium text-gray-700">Email</label>
-            <input type="email" name="email" id="email" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
-            @error('email')
-                <span class="text-red-500 text-sm">{{ $message }}</span>
+            <label for="class" class="block font-medium text-gray-700">Klas</label>
+            <select name="class" id="class" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
+                <option value="">Klas naam</option>
+                <option value="class1">klas 1</option>
+                <option value="class2">klas 2</option>
+                <option value="class3">klas 3</option>
+            </select>
+            @error('class')
+            <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
-            <!-- phone -->
 
-        <div class="mb-4">
+        <!-- phone -->
+
+        <!-- <div class="mb-4">
             <label for="phone" class="block font-medium text-gray-700">Phone</label>
             <input type="tel" name="phone" id="phone" class="border-gray-300 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border rounded-md shadow-sm p-2">
             @error('phone')
-                <span class="text-red-500 text-sm">{{ $message }}</span>
+            <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
-        </div>
+        </div> -->
 
 
-        <div class="mb-4">
-            <button type="submit" class="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600">Submit</button>
+        <div class="mb-4 w-full">
+            <button type="submit" class="bg-[#019AAC] text-white py-2 px-4 rounded hover:bg-indigo-600 w-full text-center ">Register</button>
         </div>
         <div>
-            <a href="{{ url('/students') }}" class="text-blue-500">Back</a>
+            <a href="{{ url('/students') }}" class="text-blue-500">Terug</a>
         </div>
     </form>
 </div>
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 
 <div class="p-4">
-        <h3 class="text-xl font-bold mb-4 text-center my-4 ">Students crud Program</h3>
         <a href="{{ route('student.create') }}" class="w-3/4 mx-auto bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">Add new Student</a>
     </div>
 
@@ -10,42 +9,28 @@
     <div class="w-3/4 mx-auto">
         {{ $Students->links()  }}
     </div>
-        <thead>
-            <tr>
-                <th class="px-4 py-2">ID</th>
-                <th class="px-4 py-2">firstname</th>
-                <th class="px-4 py-2">lastname</th>
-                <th class="px-4 py-2">email</th>
-                <th class="px-4 py-2">phone</th>
-                <th class="px-4 py-2">Actions</th>
-            </tr>
-        </thead>
+       
 
         <tbody>
             @foreach($Students as $Student)
-                <tr>
-                    <td class="border px-4 py-2">{{ $Student['id'] }}</td>
-                    <td class="border px-4 py-2">{{ $Student['firstname'] }}</td>
-                    <td class="border px-4 py-2">{{ $Student['lastname'] }}</td>
-                    <td class="border px-4 py-2">{{ $Student['email'] }}</td>
-
-                    <td class="border px-4 py-2">{{ $Student['phone'] }}</td>
-                    <td class="border px-4 py-2">
+                <tr class="border-b flex flex-row justify-between ">
+                    <td class=" px-4 py-2">{{ $Student['firstname'] }}</td>
+                    <td class=" px-4 py-2">
     <button class="bg-blue-500 text-white py-1 px-2 rounded">
         <a href="{{ route('student.edit', $Student->id) }}" class="text-white">
-            <i class="fas fa-edit"></i> Edit
+            <i class="fas fa-edit"></i> Update
         </a>
     </button>
-    <button class="bg-yellow-500 text-white py-1 px-2 rounded">
+    <!-- <button class="bg-yellow-500 text-white py-1 px-2 rounded">
         <a href="{{ route('student.show', $Student->id) }}" class="text-white">
             <i class="fas fa-eye"></i> View
         </a>
-    </button>
+    </button> -->
     <form action="{{ url('/students/' . $Student->id) }}" method="post" class="inline">
         @csrf 
         @method('DELETE')
         <button class="bg-red-500 text-white py-1 px-2 rounded" type="submit">
-            <i class="fas fa-trash"></i> Delete
+            <i class="fas fa-trash"></i> verwijderen
         </button>
     </form>
 </td>
