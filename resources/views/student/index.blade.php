c76b7d286f9a23eb2c2662a6eaf1e7eb0ac15181
Thimo <thimodehaan@gmail.com>
2025-01-22 19:06:20 +0100

    Updated nam for students to kinderen

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,31 +2,31 @@
 @section('content')
 
 <div class="p-4">
-        <a href="{{ route('student.create') }}" class="w-3/4 mx-auto bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">Add new Student</a>
+        <a href="{{ route('kinderen.create') }}" class="w-3/4 mx-auto bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">Add new Kind</a>
     </div>
 
     <table class="w-3/4 mx-auto">
     <div class="w-3/4 mx-auto">
-        {{ $Students->links()  }}
+        {{ $Kinderen->links()  }}
     </div>
        
 
         <tbody>
-            @foreach($Students as $Student)
+            @foreach($Kinderen as $Kind)
                 <tr class="border-b flex flex-row justify-between ">
-                    <td class=" px-4 py-2">{{ $Student['firstname'] }}</td>
+                    <td class=" px-4 py-2">{{ $Kind['firstname'] }}</td>
                     <td class=" px-4 py-2">
     <button class="bg-blue-500 text-white py-1 px-2 rounded">
-        <a href="{{ route('student.edit', $Student->id) }}" class="text-white">
+        <a href="{{ route('kinderen.edit', $Kind->UUID) }}" class="text-white">
             <i class="fas fa-edit"></i> Update
         </a>
     </button>
     <!-- <button class="bg-yellow-500 text-white py-1 px-2 rounded">
-        <a href="{{ route('student.show', $Student->id) }}" class="text-white">
+        <a href="{{ route('kinderen.show', $Kind->UUID) }}" class="text-white">
             <i class="fas fa-eye"></i> View
         </a>
     </button> -->
-    <form action="{{ url('/students/' . $Student->id) }}" method="post" class="inline">
+    <form action="{{ url('/kinderen/' . $Kind->UUID) }}" method="post" class="inline">
         @csrf 
         @method('DELETE')
         <button class="bg-red-500 text-white py-1 px-2 rounded" type="submit">
