731360e9cb10724e9df002569b4d3da1d30f7760
Thimo <thimodehaan@gmail.com>
2025-01-28 01:33:44 +0100

    updated terug know in show van voorwerpen

--- resources/views/voorwerpen/show.blade.php
+++ resources/views/voorwerpen/show.blade.php
@@ -60,7 +60,7 @@
     </div>
 
     <div class="mt-4">
-        <a href="{{ url('/voorwerpen') }}" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Back</a>
+        <a href="{{ url('/voorwerpen') }}" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700">Terug</a>
     </div>
 </div>
 @endsection
